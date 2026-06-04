afaece72725230363f478523e849cf827156919e
Vishal <vgvishal31@gmail.com>
2026-06-05 01:26:05 +0530

    fix: remove outputFileTracingRoot, lazy load datasets

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../.."),
   basePath: "/stat-engine"
 };
 
